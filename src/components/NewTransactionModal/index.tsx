937e67044f552310d52af07979d7507d7024e2d7
Salinas <matheussalinas3@gmail.com>
2022-11-17 14:20:24 -0300

    Estrutura visual - Botões de entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { CloseButton, Content, Overlay } from './style'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './style'
 
 export function NewTransactionModal() {
   return (
@@ -16,6 +22,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              <span>Entrada</span>
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              <span>Saída</span>
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
